8f173b4714366ee33a0b4ef3cea4931eaec94f9b
slamhan <slamhan1987@hotmail.com>
2017-10-13 23:32:04 +0800

    refactor

--- auth-service/src/main/java/com/piggymetrics/auth/AuthApplication.java
+++ auth-service/src/main/java/com/piggymetrics/auth/AuthApplication.java
@@ -86,6 +86,7 @@ public class AuthApplication {
 
             // TODO persist clients details
 
+            System.out.println("xxxxxxxxxxxxxxxxxxxxxx"+env.getProperty("ACCOUNT_SERVICE_PASSWORD"));
             // @formatter:off
             clients.inMemory()
                     .withClient("browser")
@@ -93,7 +94,7 @@ public class AuthApplication {
                     .scopes("ui")
                     .and()
                     .withClient("account-service")
-                    .secret(env.getProperty("ACCOUNT_SERVICE_PASSWORD"))
+//                    .secret(env.getProperty("ACCOUNT_SERVICE_PASSWORD"))
                     .authorizedGrantTypes("client_credentials", "refresh_token")
                     .scopes("server")
                     .and()
