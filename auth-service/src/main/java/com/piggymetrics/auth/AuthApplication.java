7672167ff85f6a90efddf35abe1be5c4c0c4a095
slamhan <slamhan1987@hotmail.com>
2017-10-14 17:59:36 +0800

    filter-order: 3 https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-1.5-Release-Notes#oauth-2-resource-filter
    OAuth 2 Resource Filter
    
    The default order of the OAuth2 resource filter has changed from 3 to SecurityProperties.ACCESS_OVERRIDE_ORDER - 1. This places it after the actuator endpoints but before the basic authentication filter chain. The default can be restored by setting security.oauth2.resource.filter-order = 3

--- auth-service/src/main/java/com/piggymetrics/auth/AuthApplication.java
+++ auth-service/src/main/java/com/piggymetrics/auth/AuthApplication.java
@@ -28,8 +28,8 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @SpringBootApplication
-//@EnableResourceServer
-//@EnableDiscoveryClient
+@EnableResourceServer
+@EnableDiscoveryClient
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AuthApplication {
 
