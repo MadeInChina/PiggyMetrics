230f70751c9969046821ad31b9a59e764c884f20
slamhan <slamhan1987@hotmail.com>
2017-10-14 21:34:46 +0800

    @EnableCircuitBreaker

--- monitoring-server/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
+++ monitoring-server/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
@@ -2,11 +2,13 @@ package com.piggymetrics.monitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
 @EnableTurbineStream
+@EnableCircuitBreaker
 @EnableHystrixDashboard
 public class MonitoringApplication {
 
