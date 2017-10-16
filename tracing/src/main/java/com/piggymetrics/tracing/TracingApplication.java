812b947b2732d7376eab9a6459bcb13d0d2290d9
slamhan <slamhan1987@hotmail.com>
2017-10-16 14:40:55 +0800

    Add tracing module

---
+++ tracing/src/main/java/com/piggymetrics/tracing/TracingApplication.java
@@ -0,0 +1,14 @@
+package com.piggymetrics.tracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootApplication
+public class TracingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TracingApplication.class, args);
+    }
+}
