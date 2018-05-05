4dfb21d7d07e72f59c19454c13730ae35d423f94
slamhan <slamhan1987@hotmail.com>
2018-05-05 14:01:30 +0800

    fixed issue.

--- monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
+++ monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
-import org.springframework.cloud.stream.converter.CompositeMessageConverterFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.integration.support.converter.ConfigurableCompositeMessageConverter;
 
 @SpringBootApplication
 @EnableTurbineStream
@@ -20,12 +17,5 @@ public class MonitoringApplication {
     application.setBannerMode(Banner.Mode.OFF);
     application.setWebApplicationType(WebApplicationType.SERVLET);
     application.run(args);
-  }
-
-  @Bean
-  public ConfigurableCompositeMessageConverter integrationArgumentResolverMessageConverter(
-      CompositeMessageConverterFactory factory) {
-    return new ConfigurableCompositeMessageConverter(
-        factory.getMessageConverterForAllRegistered().getConverters());
   }
 }
