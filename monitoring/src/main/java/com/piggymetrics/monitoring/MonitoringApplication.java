53acc70ab7221ae4520bab2dd138160ea60a92fb
slamhan <slamhan1987@hotmail.com>
2018-05-05 13:11:05 +0800

    fixed bugs.

--- monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
+++ monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
@@ -17,13 +17,13 @@ public class MonitoringApplication {
     SpringApplication.run(MonitoringApplication.class, args);
   }
 
-  @Bean // HystrixDashboard监控需要的servlet，没有自动注册，需要手动注入
-  ServletRegistrationBean getServlet() {
-    HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
-    ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(streamServlet);
-    servletRegistrationBean.setLoadOnStartup(1);
-    servletRegistrationBean.addUrlMappings("/hystrix.stream");
-    servletRegistrationBean.setName("HystrixMetricsStreamServlet");
-    return servletRegistrationBean;
-  }
+//  @Bean // HystrixDashboard监控需要的servlet，没有自动注册，需要手动注入
+//  ServletRegistrationBean getServlet() {
+//    HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+//    ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(streamServlet);
+//    servletRegistrationBean.setLoadOnStartup(1);
+//    servletRegistrationBean.addUrlMappings("/hystrix.stream");
+//    servletRegistrationBean.setName("HystrixMetricsStreamServlet");
+//    return servletRegistrationBean;
+//  }
 }
