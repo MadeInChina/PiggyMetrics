8fded62a6127e2c2242b5d9e01c692cad99ca6fb
slamhan <slamhan1987@hotmail.com>
2018-05-05 20:38:23 +0800

    application.setWebApplicationType(WebApplicationType.SERVLET);

--- monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
+++ monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
@@ -1,6 +1,8 @@
 package com.piggymetrics.monitoring;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
@@ -14,7 +16,10 @@ import org.springframework.integration.support.converter.ConfigurableCompositeMe
 public class MonitoringApplication {
   // for spring-boot-2.0,x using host:port/actuator/hystrix.stream
   public static void main(String[] args) {
-    SpringApplication.run(MonitoringApplication.class, args);
+    final SpringApplication application = new SpringApplication(MonitoringApplication.class);
+    application.setBannerMode(Banner.Mode.OFF);
+    application.setWebApplicationType(WebApplicationType.SERVLET);
+    application.run(args);
   }
 
   @Bean
