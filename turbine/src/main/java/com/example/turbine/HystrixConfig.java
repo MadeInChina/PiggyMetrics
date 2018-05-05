f9880cc9f27db888f5ad34c97930c0f96c99c1ac
slamhan <slamhan1987@hotmail.com>
2018-05-06 00:30:18 +0800

    separated into turbine-service.

---
+++ turbine/src/main/java/com/example/turbine/HystrixConfig.java
@@ -0,0 +1,24 @@
+package com.example.turbine;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HystrixConfig {
+
+  @Bean
+  public HystrixMetricsStreamServlet hystrixMetricsStreamServlet() {
+    return new HystrixMetricsStreamServlet();
+  }
+
+  @Bean
+  public ServletRegistrationBean<HystrixMetricsStreamServlet> registration(
+      HystrixMetricsStreamServlet servlet) {
+    ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean =
+        new ServletRegistrationBean<>(servlet, "/actuator/hystrix.stream");
+    registrationBean.setEnabled(true);
+    return registrationBean;
+  }
+}
