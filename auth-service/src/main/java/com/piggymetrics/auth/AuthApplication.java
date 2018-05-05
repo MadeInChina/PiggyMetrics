1e193b54c9a396c2d18f28943848691f27777e09
slamhan <slamhan1987@hotmail.com>
2018-05-05 11:53:22 +0800

    Troubleshooting java.lang.IllegalArgumentException: There is no PasswordEncoder mapped for the id "null"                 	at org.springframework.security.crypto.password.DelegatingPasswordEncoder$UnmappedIdPasswordEncoder.matches(DelegatingPasswordEncoder.java:233)                 	at org.springframework.security.crypto.password.DelegatingPasswordEncoder.matches(DelegatingPasswordEncoder.java:196)

--- auth-service/src/main/java/com/piggymetrics/auth/AuthApplication.java
+++ auth-service/src/main/java/com/piggymetrics/auth/AuthApplication.java
@@ -5,11 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -18,6 +16,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -33,97 +33,96 @@ import org.springframework.security.oauth2.provider.token.store.InMemoryTokenSto
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AuthApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AuthApplication.class, args);
+  public static void main(String[] args) {
+    SpringApplication.run(AuthApplication.class, args);
+  }
+
+  @Configuration
+  @EnableWebSecurity
+  protected static class webSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired private MongoUserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      // @formatter:off
+      http.authorizeRequests().anyRequest().authenticated().and().csrf().disable();
+      // @formatter:on
     }
 
-    @Configuration
-    @EnableWebSecurity
-    protected static class webSecurityConfig extends WebSecurityConfigurerAdapter {
-
-        @Autowired
-        private MongoUserDetailsService userDetailsService;
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            // @formatter:off
-            http
-                    .authorizeRequests().anyRequest().authenticated()
-                    .and()
-                    .csrf().disable();
-            // @formatter:on
-        }
-
-        @Override
-        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth.userDetailsService(userDetailsService)
-                    .passwordEncoder(new BCryptPasswordEncoder());
-        }
-
-        @Override
-        @Bean
-        public AuthenticationManager authenticationManagerBean() throws Exception {
-            return super.authenticationManagerBean();
-        }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+      auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
-    @Configuration
-    @EnableAuthorizationServer
-    protected static class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
-
-        private TokenStore tokenStore = new InMemoryTokenStore();
-
-        @Autowired
-        @Qualifier("authenticationManagerBean")
-        private AuthenticationManager authenticationManager;
-
-        @Autowired
-        private MongoUserDetailsService userDetailsService;
-
-        @Autowired
-        private Environment env;
-
-        @Override
-        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-
-            // TODO persist clients details
-
-            // @formatter:off
-            clients.inMemory()
-                    .withClient("browser")
-                    .authorizedGrantTypes("refresh_token", "password")
-                    .scopes("ui")
-                    .and()
-                    .withClient("account-service")
-                    .secret(env.getProperty("ACCOUNT_SERVICE_PASSWORD"))
-                    .authorizedGrantTypes("client_credentials", "refresh_token")
-                    .scopes("server")
-                    .and()
-                    .withClient("statistics-service")
-                    .secret(env.getProperty("STATISTICS_SERVICE_PASSWORD"))
-                    .authorizedGrantTypes("client_credentials", "refresh_token")
-                    .scopes("server")
-                    .and()
-                    .withClient("notification-service")
-                    .secret(env.getProperty("NOTIFICATION_SERVICE_PASSWORD"))
-                    .authorizedGrantTypes("client_credentials", "refresh_token")
-                    .scopes("server");
-            // @formatter:on
-        }
-
-        @Override
-        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-            endpoints
-                    .tokenStore(tokenStore)
-                    .authenticationManager(authenticationManager)
-                    .userDetailsService(userDetailsService);
-        }
-
-        @Override
-        public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-            oauthServer
-                    .tokenKeyAccess("permitAll()")
-                    .checkTokenAccess("isAuthenticated()");
-        }
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+      return super.authenticationManagerBean();
     }
+  }
+
+  @Configuration
+  @EnableAuthorizationServer
+  protected static class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+
+    private TokenStore tokenStore = new InMemoryTokenStore();
+
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    private AuthenticationManager authenticationManager;
+
+    @Autowired private MongoUserDetailsService userDetailsService;
+
+    @Autowired private Environment env;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+      // TODO persist clients details
+
+      // @formatter:off
+      clients
+          .inMemory()
+          .withClient("browser")
+          .authorizedGrantTypes("refresh_token", "password")
+          .scopes("ui")
+          .and()
+          .withClient("account-service")
+          .secret(env.getProperty("ACCOUNT_SERVICE_PASSWORD"))
+          .authorizedGrantTypes("client_credentials", "refresh_token")
+          .scopes("server")
+          .and()
+          .withClient("statistics-service")
+          .secret(env.getProperty("STATISTICS_SERVICE_PASSWORD"))
+          .authorizedGrantTypes("client_credentials", "refresh_token")
+          .scopes("server")
+          .and()
+          .withClient("notification-service")
+          .secret(env.getProperty("NOTIFICATION_SERVICE_PASSWORD"))
+          .authorizedGrantTypes("client_credentials", "refresh_token")
+          .scopes("server");
+      // @formatter:on
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+      endpoints
+          .tokenStore(tokenStore)
+          .authenticationManager(authenticationManager)
+          .userDetailsService(userDetailsService);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) {
+      oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+    }
+  }
+
+  // Troubleshooting https://docs.spring.io/spring-security/site/docs/current/reference/htmlsingle/#getting-started-experience
+  // java.lang.IllegalArgumentException: There is no PasswordEncoder mapped for the id "null"
+  @Bean
+  public static PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
 }
