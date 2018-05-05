f3eda29b59a7c2e8a78b5abe8a1d54658ba91cbc
slamhan <slamhan1987@hotmail.com>
2018-05-05 16:36:04 +0800

    refactor

--- monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
+++ monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
@@ -3,18 +3,18 @@ package com.piggymetrics.monitoring;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 import org.springframework.cloud.stream.converter.CompositeMessageConverterFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.support.converter.ConfigurableCompositeMessageConverter;
 
-@SpringCloudApplication
+@SpringBootApplication
 @EnableTurbineStream
 @EnableHystrixDashboard
 public class MonitoringApplication {
-  //for spring-boot-2.0,x using host:port/actuator/hystrix.stream
+  // for spring-boot-2.0,x using host:port/actuator/hystrix.stream
   public static void main(String[] args) {
     final SpringApplication application = new SpringApplication(MonitoringApplication.class);
     application.setBannerMode(Banner.Mode.OFF);
@@ -25,7 +25,7 @@ public class MonitoringApplication {
   @Bean
   public ConfigurableCompositeMessageConverter integrationArgumentResolverMessageConverter(
       CompositeMessageConverterFactory factory) {
-    return new ConfigurableCompositeMessageConverter(factory.getMessageConverterForAllRegistered().getConverters());
+    return new ConfigurableCompositeMessageConverter(
+        factory.getMessageConverterForAllRegistered().getConverters());
   }
-
 }
