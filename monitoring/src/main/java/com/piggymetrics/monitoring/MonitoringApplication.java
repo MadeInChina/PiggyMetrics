3a268f325d3eb15a48426dbf374f768a234a1093
slamhan <slamhan1987@hotmail.com>
2018-05-05 16:04:24 +0800

    management:   endpoints:     web:       exposure:         include: ["health", "info", "hystrix.stream"]

--- monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
+++ monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
@@ -1,16 +1,21 @@
 package com.piggymetrics.monitoring;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
-@SpringBootApplication
+@SpringCloudApplication
 @EnableTurbineStream
 @EnableHystrixDashboard
 public class MonitoringApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(MonitoringApplication.class, args);
+    final SpringApplication application = new SpringApplication(MonitoringApplication.class);
+    application.setBannerMode(Banner.Mode.OFF);
+    application.setWebApplicationType(WebApplicationType.SERVLET);
+    application.run(args);
   }
 }
