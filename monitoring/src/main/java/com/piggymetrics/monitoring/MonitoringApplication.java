10a144ec940965dadd4efd6034b41c926f300e84
slamhan <slamhan1987@hotmail.com>
2018-05-05 13:27:14 +0800

    add servlet.

--- monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
+++ monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
@@ -1,12 +1,11 @@
 package com.piggymetrics.monitoring;
 
-import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableTurbineStream
@@ -14,16 +13,9 @@ import org.springframework.context.annotation.Bean;
 public class MonitoringApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(MonitoringApplication.class, args);
+    final SpringApplication application = new SpringApplication(MonitoringApplication.class);
+    application.setBannerMode(Banner.Mode.OFF);
+    application.setWebApplicationType(WebApplicationType.SERVLET);
+    application.run(args);
   }
-
-//  @Bean // HystrixDashboard监控需要的servlet，没有自动注册，需要手动注入
-//  ServletRegistrationBean getServlet() {
-//    HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
-//    ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(streamServlet);
-//    servletRegistrationBean.setLoadOnStartup(1);
-//    servletRegistrationBean.addUrlMappings("/hystrix.stream");
-//    servletRegistrationBean.setName("HystrixMetricsStreamServlet");
-//    return servletRegistrationBean;
-//  }
 }
