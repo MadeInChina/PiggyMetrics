59a148ed36ee1f94ec27a9b995e68b190ea600c7
slamhan <slamhan1987@hotmail.com>
2018-05-05 15:43:51 +0800

    fix com.fasterxml.jackson.core.JsonParseException: Unexpected character (',' (code 44)): Expected space separating root-level values      at [Source: (String)"123,34,111,114,105,103,105,110,34,58,123,34,104,111,115,116,34,58,34,49,48,46,52,50,46,49,53,50,46,54,56,34,44,34,112,111,114,116,34,58,54,48,48,48,44,34,115,101,114,118,105,99,101,73,100,34,58,34,97,99,99,111,117,110,116,45,115,101,114,118,105,99,101,34,44,34,105,100,34,58,34,97,99,99,111,117,110,116,45,115,101,114,118,105,99,101,45,49,34,125,44,34,100,97,116,97,34,58,123,34,116,121,112,101,34,58,34,72,121,115,116,114,105,120,84,104,114,101,97,100,80,111,111,108,34,44,34,110,97,109,101,34,58,34"[trun

--- monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
+++ monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
@@ -1,8 +1,6 @@
 package com.piggymetrics.monitoring;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
@@ -13,9 +11,6 @@ import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 public class MonitoringApplication {
 
   public static void main(String[] args) {
-    final SpringApplication application = new SpringApplication(MonitoringApplication.class);
-    application.setBannerMode(Banner.Mode.OFF);
-    application.setWebApplicationType(WebApplicationType.SERVLET);
-    application.run(args);
+    SpringApplication.run(MonitoringApplication.class, args);
   }
 }
