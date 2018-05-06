b6757ad18280352d77ad35ff85197edcf023d1b3
slamhan <slamhan1987@hotmail.com>
2018-05-06 20:12:27 +0800

    remove hystrix config.not using now.

--- monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
+++ monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
@@ -1,9 +1,7 @@
 package com.piggymetrics.monitoring;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
@@ -11,11 +9,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @SpringBootApplication
 @EnableHystrixDashboard
 public class MonitoringApplication {
-  // for spring-boot-2.0,x using host:port/actuator/hystrix.stream
   public static void main(String[] args) {
-    final SpringApplication application = new SpringApplication(MonitoringApplication.class);
-    application.setBannerMode(Banner.Mode.OFF);
-    application.setWebApplicationType(WebApplicationType.SERVLET);
-    application.run(args);
+    SpringApplication.run(MonitoringApplication.class, args);
   }
 }
