4f96cacfe771f05fa2f9bc68f6526099ea8f7a2f
slamhan <slamhan1987@hotmail.com>
2018-05-06 13:29:18 +0800

    separated into turbine-service.

--- turbine/src/main/java/com/example/turbine/TurbineApplication.java
+++ turbine/src/main/java/com/example/turbine/TurbineApplication.java
@@ -1,9 +1,6 @@
 package com.example.turbine;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
@@ -14,14 +11,10 @@ import org.springframework.integration.support.converter.ConfigurableCompositeMe
 @EnableDiscoveryClient
 @EnableTurbineStream
 @SpringBootApplication
-@EnableAutoConfiguration
 public class TurbineApplication {
 
   public static void main(String[] args) {
-    final SpringApplication application = new SpringApplication(TurbineApplication.class);
-    application.setBannerMode(Banner.Mode.OFF);
-    application.setWebApplicationType(WebApplicationType.SERVLET);
-    application.run(args);
+    SpringApplication.run(TurbineApplication.class, args);
   }
 
   @Bean
