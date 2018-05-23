2a727adb5791956dfd1cfe7f701bc99b1c79cfb6
slamhan <slamhan1987@hotmail.com>
2018-05-23 12:01:26 +0800

    update dependencies

--- infrastructure-services/turbine/src/main/java/com/example/turbine/TurbineApplication.java
+++ infrastructure-services/turbine/src/main/java/com/example/turbine/TurbineApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
-import org.springframework.cloud.stream.converter.CompositeMessageConverterFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.integration.support.converter.ConfigurableCompositeMessageConverter;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -15,12 +12,5 @@ public class TurbineApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(TurbineApplication.class, args);
-  }
-
-  @Bean
-  public ConfigurableCompositeMessageConverter integrationArgumentResolverMessageConverter(
-      CompositeMessageConverterFactory factory) {
-    return new ConfigurableCompositeMessageConverter(
-        factory.getMessageConverterForAllRegistered().getConverters());
   }
 }
