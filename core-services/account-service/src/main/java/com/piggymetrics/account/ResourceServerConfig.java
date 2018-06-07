3b3edba857cd18e43a2322101cee543ae19a2eff
slamhan <slamhan1987@hotmail.com>
2018-06-07 17:14:47 +0800

    add jwt token

--- core-services/account-service/src/main/java/com/piggymetrics/account/ResourceServerConfig.java
+++ core-services/account-service/src/main/java/com/piggymetrics/account/ResourceServerConfig.java
@@ -1,19 +1,13 @@
 package com.piggymetrics.account;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
-
-  @Autowired RemoteTokenServices remoteTokenServices;
 
   @Override
   public void configure(HttpSecurity http) throws Exception {
@@ -22,11 +16,5 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         .permitAll()
         .anyRequest()
         .authenticated();
-  }
-
-  @Bean
-  @Primary
-  public RemoteTokenServices tokenServices() {
-    return remoteTokenServices;
   }
 }
