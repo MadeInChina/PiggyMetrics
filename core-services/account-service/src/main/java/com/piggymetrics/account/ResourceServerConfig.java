e39372c12bc8bcf99ac0c292d5337925d1cd88b1
slamhan <slamhan1987@hotmail.com>
2018-06-07 17:06:58 +0800

    add jwt token

--- core-services/account-service/src/main/java/com/piggymetrics/account/ResourceServerConfig.java
+++ core-services/account-service/src/main/java/com/piggymetrics/account/ResourceServerConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -33,6 +34,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 //  public ResourceServerTokenServices tokenServices() {
 //    return new CustomUserInfoTokenServices(sso.getUserInfoUri(), sso.getClientId());
 //  }
+
+  @Override
+  public void configure(ResourceServerSecurityConfigurer config) {
+    config.tokenServices(tokenServices());
+  }
 
   @Bean
   public JwtTokenStore tokenStore() {
