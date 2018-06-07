5e822a2d6ee9d5a53f7afd1621fef4a7a80d757a
slamhan <slamhan1987@hotmail.com>
2018-06-07 17:13:55 +0800

    add jwt token

--- core-services/account-service/src/main/java/com/piggymetrics/account/ResourceServerConfig.java
+++ core-services/account-service/src/main/java/com/piggymetrics/account/ResourceServerConfig.java
@@ -1,25 +1,19 @@
 package com.piggymetrics.account;
 
-import com.piggymetrics.account.service.security.CustomUserInfoTokenServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-  @Autowired private ResourceServerProperties sso;
+  @Autowired RemoteTokenServices remoteTokenServices;
 
   @Override
   public void configure(HttpSecurity http) throws Exception {
@@ -30,36 +24,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         .authenticated();
   }
 
-//  @Bean
-//  public ResourceServerTokenServices tokenServices() {
-//    return new CustomUserInfoTokenServices(sso.getUserInfoUri(), sso.getClientId());
-//  }
-
-  @Override
-  public void configure(ResourceServerSecurityConfigurer config) {
-    config.tokenServices(tokenServices());
-  }
-
-  @Bean
-  public JwtTokenStore tokenStore() {
-
-    return new JwtTokenStore(accessTokenConverter());
-  }
-
-  @Bean
-  public JwtAccessTokenConverter accessTokenConverter() {
-    JwtAccessTokenConverter enhancer = new JwtAccessTokenConverter();
-    enhancer.setSigningKey("123456");
-    return enhancer;
-  }
-
   @Bean
   @Primary
-  public DefaultTokenServices tokenServices() {
-    DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-    defaultTokenServices.setTokenStore(tokenStore());
-    defaultTokenServices.setSupportRefreshToken(true);
-    defaultTokenServices.setTokenEnhancer(accessTokenConverter());
-    return defaultTokenServices;
+  public RemoteTokenServices tokenServices() {
+    return remoteTokenServices;
   }
 }
