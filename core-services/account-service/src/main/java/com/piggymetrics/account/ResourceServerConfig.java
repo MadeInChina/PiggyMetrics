cde7afc4681349d668edc888cf7ab81111ec9b9d
slamhan <slamhan1987@hotmail.com>
2018-06-07 17:01:51 +0800

    add jwt token

--- core-services/account-service/src/main/java/com/piggymetrics/account/ResourceServerConfig.java
+++ core-services/account-service/src/main/java/com/piggymetrics/account/ResourceServerConfig.java
@@ -5,10 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableResourceServer
@@ -25,8 +29,31 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         .authenticated();
   }
 
+//  @Bean
+//  public ResourceServerTokenServices tokenServices() {
+//    return new CustomUserInfoTokenServices(sso.getUserInfoUri(), sso.getClientId());
+//  }
+
   @Bean
-  public ResourceServerTokenServices tokenServices() {
-    return new CustomUserInfoTokenServices(sso.getUserInfoUri(), sso.getClientId());
+  public JwtTokenStore tokenStore() {
+
+    return new JwtTokenStore(accessTokenConverter());
+  }
+
+  @Bean
+  public JwtAccessTokenConverter accessTokenConverter() {
+    JwtAccessTokenConverter enhancer = new JwtAccessTokenConverter();
+    enhancer.setSigningKey("123456");
+    return enhancer;
+  }
+
+  @Bean
+  @Primary
+  public DefaultTokenServices tokenServices() {
+    DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+    defaultTokenServices.setTokenStore(tokenStore());
+    defaultTokenServices.setSupportRefreshToken(true);
+    defaultTokenServices.setTokenEnhancer(accessTokenConverter());
+    return defaultTokenServices;
   }
 }
